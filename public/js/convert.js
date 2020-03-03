e47ff554cdb3cbf1dfacca17f4f415a85c44317a
Marvel-Ib <awesomewonder553@gmail.com>
2020-03-03 15:08:18 +0100

    done with validating in semantic ui

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,4 +1,26 @@
-console.log("how far");
+$('select.dropdown')
+  .dropdown()
+;
 
-console.log("sdf");
-
+$('form').form({
+    on: 'blur',
+    keyboardShortcuts: false,
+    fields: {
+        empty: {
+            identifier: 'empty',
+            rules: [{
+                type: 'empty',
+                prompt: 'Please enter a value'
+            }]
+        },
+        dropdown: {
+            identifier: 'dropdown',
+            rules: [{
+                type: 'empty',
+                prompt: 'Please select a dropdown value'
+            }]
+        }
+        
+    }
+});
+// <div class="ui loading form">
